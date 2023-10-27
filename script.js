df3c73bc0b027e89e1df92bfdca3fbf2968cc7d5
Jason A. <you@example.com>
2023-10-27 16:37:58 -0300

    header @media adjust

---
+++ script.js
@@ -0,0 +1,7 @@
+//EXPANDIR MENU
+var btnExp = document.querySelector('.btn-buscar')
+var menuForm = document.querySelector('.formulario')
+
+btnExp.addEventListener('click', function(){
+    menuForm.classList.toggle('expandir')
+})
