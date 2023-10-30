ede0c2de961a009323b97bb88ee4e712ec2cdc71
Jason A. <you@example.com>
2023-10-30 11:50:38 -0300

    adjusts main elements

--- script.js
+++
@@ -1,7 +0,0 @@
-//EXPANDIR MENU
-var btnExp = document.querySelector('.btn-buscar')
-var menuForm = document.querySelector('.formulario')
-
-btnExp.addEventListener('click', function(){
-    menuForm.classList.toggle('expandir')
-})
